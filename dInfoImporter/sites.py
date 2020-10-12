85c8036bf4f951aad31b984a73a42699eb9bc8dd
layolu layole <layolulayole@gmail.com>
2020-10-12 17:49:11 +0900

    fixed a bug with no thumbnail pages for Melonbook; rewrote some redundant bool statements

--- dInfoImporter/sites.py
+++ dInfoImporter/sites.py
@@ -58,7 +58,7 @@ class Melonbooks(Site):
         d.size = td_extractor('版型・メディア').text.strip()
         d.pages = int(td_extractor('総ページ数・CG数・曲数').text.strip())
         d.convention_name = td_extractor('イベント').text.strip()
-        d.is_adult = True if td_extractor('作品種別').text.strip() == '18禁' else False
+        d.is_adult = td_extractor('作品種別').text.strip() == '18禁'
 
         # TODO: melonbooks also sells copybooks, but it seems hard to determine each book is it or not
         d.is_copybook = False
@@ -68,14 +68,16 @@ class Melonbooks(Site):
         d.general_tags = [li.text for li in tags]
         d.coupling_names = []
         d.character_names = []
-        d.is_novel = True if '小説' in d.general_tags else False
-        d.is_anthology = True if 'アンソロジー' in d.general_tags else False
+        d.is_novel = '小説' in d.general_tags
+        d.is_anthology = 'アンソロジー' in d.general_tags
 
         d.cover_url = 'https:' + soup.find('a', class_='tag_sample1')['href']
-        d.sample_urls = ['https:' + a['href'] for a in soup.select_one('#thumbs').ul.find_all('a')]
-
         d.cover_image = self.ses.get(d.cover_url).content
-        d.sample_images = [self.ses.get(url).content for url in d.sample_urls]
+
+        div_thumbs = soup.select_one('#thumbs')
+        if div_thumbs is not None:
+            d.sample_urls = ['https:' + a['href'] for a in div_thumbs.ul.find_all('a')]
+            d.sample_images = [self.ses.get(url).content for url in d.sample_urls]
         return d
 
 
@@ -100,7 +102,7 @@ class Toranoana(Site):
         d.circle_name = soup.find('div', class_='sub-circle').find('span', class_='sub-p').a.text.strip()
         d.artist_names = [a.text.strip() for a in soup.find('div', class_='sub-name').find_all('span', class_='sub-p')]
         
-        d.is_adult = True if soup.find('span', class_='mk-rating') else False
+        d.is_adult = soup.find('span', class_='mk-rating') is not None
         # TODO: because the definition of 'anthology' is not clear in the site, set to False for the moment
         d.is_anthology = False
 
@@ -131,8 +133,8 @@ class Toranoana(Site):
         td_type = td_extractor('種別/サイズ')
         if td_type is not None:
             type_list = td_type.text.replace('-', '').replace('/', '').split()
-            d.is_copybook = True if type_list[0] == 'コピー誌' else False
-            d.is_novel = True if type_list[1] == '小説' else False
+            d.is_copybook = type_list[0] == 'コピー誌'
+            d.is_novel = type_list[1] == '小説'
             d.size = jaconv.z2h(type_list[2], ascii=True, digit=True)
             if len(type_list) == 4:
                 d.pages = int(type_list[3].replace('p', '').replace(',', ''))
